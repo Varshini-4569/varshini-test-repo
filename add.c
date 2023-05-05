4e859a30163878ba1122815467520336008a7efd
Praveena.R <122393883+Praveena23-2003@users.noreply.github.com>
2023-05-05 21:34:44 +0530

    Update add.c
    added c value

--- add.c
+++ add.c
@@ -2,11 +2,11 @@
 #include<conio.h>
 void main()
 {
-int a,b,sum;
+int a,b,c,sum;
 clrscr();
 printf("enter two number:");
-scanf("%d%d",&a,&b);
-sum=a+b;
+scanf("%d%d",&a,&b,&c);
+sum=a+b+c;
 printf("%d",sum);
 getch();
 }
